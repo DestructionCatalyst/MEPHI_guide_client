c99f13fcef6c8660abedfebb30d7cc0ca5c99fd2
Владислав Комза <komza.vlad@ya.ru>
2020-03-06 23:06:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mephiguide/ui/home/HomeViewModel.java
@@ -0,0 +1,22 @@
+package com.example.mephiguide.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+    private MutableLiveData<ArrayList> listData;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/com/example/mephiguide/ui/qr/QrFragment.java
@@ -0,0 +1,38 @@
+package com.example.mephiguide.ui.qr;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mephiguide.R;
+
+public class QrFragment extends Fragment {
+
+    private QrViewModel mViewModel;
+
+    public static QrFragment newInstance() {
+        return new QrFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_qr, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(QrViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
